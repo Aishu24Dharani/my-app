89d3bab96f8cf603f436f9440ecea901e32186fd
Aishwarya <aishuasha2410@gmail.com>
2018-03-18 18:59:31 +0530

    Changed the HTML for the site.

--- itcorner/urls.py
+++ itcorner/urls.py
@@ -17,7 +17,10 @@ from django.conf.urls import include
 from django.conf.urls import url
 from django.contrib import admin
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('itb.urls')),
-]
+] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
